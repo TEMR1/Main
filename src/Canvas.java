187710dbd19d4435ee5ec8c9550c83d4626ef336
Danylo <danylo.shpak.2009@gmail.com>
2023-12-29 17:40:06 +0200

    Lesson2_13

--- src/Canvas.java
+++ src/Canvas.java
@@ -4,11 +4,6 @@ import java.awt.event.*;
 public class Canvas extends JFrame implements KeyListener {
 
     private Ninja ninja = new Ninja();
-    private FireBall fireBall = new FireBall();
-    private Monster monster1;
-    private Monster monster2;
-
-    private boolean monstersSpawned;
 
     Canvas(){
         setSize(2500,2000);
@@ -35,34 +30,6 @@ public class Canvas extends JFrame implements KeyListener {
             case KeyEvent.VK_SPACE:
                 ninja.jumpAnim();
                 break;
-            case KeyEvent.VK_ESCAPE:
-                new EndWindowGame();
-                break;
-            case KeyEvent.VK_Z:
-                monstersSpawned = true;
-
-                monster1 = new Monster(MonsterType.LARRY,1700,50);
-                monster1.setBounds(monster1.getXPosition(),monster1.getYPosition(), monster1.getWidth(), monster1.getHeight());
-
-                monster2 = new Monster(MonsterType.SLIME, 1700,250);
-                monster2.setBounds(monster2.getXPosition(), monster2.getYPosition(), monster2.getWidth(), monster2.getHeight());
-
-                add(monster1);
-                add(monster2);
-                break;
-            case KeyEvent.VK_ENTER:
-                fireBall.setVisible(true);
-
-                fireBall.setBounds(ninja.getX() + 100, ninja.getY(), fireBall.getWidth(), fireBall.getHeight());
-                add(fireBall);
-
-                fireBall.FireBallAttack();
-                break;
-            case KeyEvent.VK_M:
-                if (monstersSpawned)
-                    monster1.MoveMonsters();
-                    monster2.MoveMonsters();
-                break;
             default:
                 ninja.standAnim();
                 break;
@@ -72,6 +39,6 @@ public class Canvas extends JFrame implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent keyEvent) {
-
+        ninja.standAnim();
     }
 }
--- src/Ninja.java
+++ src/Ninja.java
@@ -7,10 +7,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Ninja extends JPanel {
-    private BaseAnimation runAnimation;
-    private BaseAnimation jumpingAnimation;
-    private BaseAnimation standAnimation;
-    private BaseAnimation currentAnimation;
+
+    private BaseAnimation currentAnimation = new BaseAnimation();
 
     public int getWidthOfStandingNinja() {
         return 141;
@@ -20,44 +18,41 @@ public class Ninja extends JPanel {
         return 227;
     }
 
-    public void standAnim(){
+    public void standAnim() {
         ArrayList<AnimPoint> points = new ArrayList<>();
-        points.add(new AnimPoint(-95,-70,141,227));
+        points.add(new AnimPoint(-95, -70, 141, 227));
 
-        standAnimation = new BaseAnimation(points,true);
-        standAnimation.start();
-        currentAnimation = standAnimation;
+        currentAnimation.addPoints(points);
+        new Thread(currentAnimation.runnable).start();
     }
 
-    public void jumpAnim(){
+    public void jumpAnim() {
         ArrayList<AnimPoint> points = new ArrayList<>();
-        points.add(new AnimPoint(-89,-560,147,333));
-        points.add(new AnimPoint(-243,-560,181,333));
-        points.add(new AnimPoint(-435,-560,185,333));
-        points.add(new AnimPoint(-631,-560,217,333));
-        points.add(new AnimPoint(-845,-560,165,333));
-        points.add(new AnimPoint(-1010,-560,211,333));
-        points.add(new AnimPoint(-1215,-560,173,333));
-        points.add(new AnimPoint(-1387,-560,171,333));
-        points.add(new AnimPoint(-1555,-560,163,333));
+        points.add(new AnimPoint(-89, -560, 147, 333));
+        points.add(new AnimPoint(-243, -560, 181, 333));
+        points.add(new AnimPoint(-435, -560, 185, 333));
+        points.add(new AnimPoint(-631, -560, 217, 333));
+        points.add(new AnimPoint(-845, -560, 165, 333));
+        points.add(new AnimPoint(-1010, -560, 211, 333));
+        points.add(new AnimPoint(-1215, -560, 173, 333));
+        points.add(new AnimPoint(-1387, -560, 171, 333));
+        points.add(new AnimPoint(-1555, -560, 163, 333));
 
-        jumpingAnimation = new BaseAnimation(points);
-        jumpingAnimation.start();
-        currentAnimation = jumpingAnimation;
+        currentAnimation.addPoints(points);
+        new Thread(currentAnimation.runnable).start();
     }
 
     public void runAnim() {
         ArrayList<AnimPoint> points = new ArrayList<>();
-        points.add(new AnimPoint(-99,-340, 200,203));
-        points.add(new AnimPoint(-311,-340,200,203));
-        points.add(new AnimPoint(-527,-340,200,203));
-        points.add(new AnimPoint(-745,-340,200,203));
-        points.add(new AnimPoint(-957,-340,200,203));
-        points.add(new AnimPoint(-1173,-340,200,203));
+        points.add(new AnimPoint(-99, -340, 200, 203));
+        points.add(new AnimPoint(-311, -340, 200, 203));
+        points.add(new AnimPoint(-527, -340, 200, 203));
+        points.add(new AnimPoint(-745, -340, 200, 203));
+        points.add(new AnimPoint(-957, -340, 200, 203));
+        points.add(new AnimPoint(-1173, -340, 200, 203));
 
-        runAnimation = new BaseAnimation(points);
-        runAnimation.start();
-        currentAnimation = runAnimation;
+        currentAnimation.addPoints(points);
+        new Thread(currentAnimation.runnable).start();
     }
 
     @Override
@@ -72,7 +67,7 @@ public class Ninja extends JPanel {
         }
     }
 
-    class BaseAnimation extends Thread {
+    class BaseAnimation {
         private int x;
         private int y;
         private int width;
@@ -94,44 +89,36 @@ public class Ninja extends JPanel {
             return height;
         }
 
-        private boolean isAllTime;
-        private ArrayList<AnimPoint> points;
+        private ArrayList<ArrayList<AnimPoint>> pointsArray = new ArrayList<>();
 
-        public BaseAnimation(ArrayList<AnimPoint> points, boolean isAllTime){
-            this.points = points;
-            this.isAllTime = isAllTime;
+        public void addPoints(ArrayList<AnimPoint> points) {
+            pointsArray.add(points);
         }
 
-        public BaseAnimation(ArrayList<AnimPoint> points){
-            this.points = points;
-        }
+        Runnable runnable = new Runnable() {
+            @Override
+            synchronized public void run() {
+                if  (pointsArray.size() == 0) {
+                    return;
+                }
+                var points = pointsArray.get(0);
+                for (int i = 0; i < points.size(); i++) {
 
-        @Override
-        public void run() {
-            int i = 0;
-            while (true) {
-                if (i == points.size()) {
-                    i = 0;
-                    if (!isAllTime){
-                        standAnim();
-                        return;
+                    x = points.get(i).getX();
+                    y = points.get(i).getY();
+                    width = points.get(i).getWidth();
+                    height = points.get(i).getHeight();
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
                     }
+
+                    repaint();
                 }
-
-                x = points.get(i).getX();
-                y = points.get(i).getY();
-                width = points.get(i).getWidth();
-                height = points.get(i).getHeight();
-
-                try {
-                    Thread.sleep(1000 / 8);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                repaint();
-                i++;
+                pointsArray.remove(0);
             }
-        }
+        };
     }
 }
